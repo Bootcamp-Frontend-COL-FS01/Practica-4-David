98162d6a505b00cf6cfe57f3647c632bd395e6ca
David Salazar <David.Salazar@jala.university>
2023-03-20 22:16:53 -0500

    resolved bug of iamges not loading when creating new post. Resolved.

--- src/newpost/newpost.ts
+++ src/newpost/newpost.ts
@@ -4,65 +4,140 @@ import { feeds } from "../feed/feed";
 import "./newpost.css"
 
 function createNewPost(title: string, content: string, author: string, imageFile?: File) {
-    const newPost: Feed = {
-        user: author,
-        title: title,
-        content: content,
-      };
-      if (imageFile) {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          if (event.target instanceof FileReader) {
-            newPost.image = event.target.result as string;
-            feeds.unshift(newPost);
-            const feedHtml = loadFeed(feeds);
-            const blogFeedElement = document.getElementById("blog-feed");
-            if (blogFeedElement) {
-              blogFeedElement.innerHTML = feedHtml;
-            }
-          }
-        };
-        reader.readAsDataURL(imageFile);
-      } else {
-        feeds.unshift(newPost);
-        const feedHtml = loadFeed(feeds);
-        const blogFeedElement = document.getElementById("blog-feed");
-        if (blogFeedElement) {
-          blogFeedElement.innerHTML = feedHtml;
-        }
+  const newPost: Feed = {
+    user: author,
+    title: title,
+    content: content,
+  };
+
+  if (imageFile) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (event.target instanceof FileReader) {
+        newPost.image = event.target.result as string;
+        updateFeedAndRedirect(newPost);
       }
-      
-    
-      // Store the updated feeds array in the localStorage
-      localStorage.setItem("feeds", JSON.stringify(feeds));
-    
+    };
+    reader.readAsDataURL(imageFile);
+  } else {
+    updateFeedAndRedirect(newPost);
   }
-  
-///////
+}
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const formElement = document.getElementById("new-post-form") as HTMLFormElement;
-      
-    formElement.addEventListener("submit", (event) => {
-      event.preventDefault();
-      const title = (document.getElementById("title") as HTMLInputElement).value;
-      const content = (document.getElementById("content") as HTMLTextAreaElement).value;
-      const author = (document.getElementById("author") as HTMLInputElement).value;
-      const imageFile = (document.getElementById("image") as HTMLInputElement).files?.[0];
-      createNewPost(title, content, author, imageFile);
-      alert('Post submitted'); 
+function updateFeedAndRedirect(newPost: Feed) {
+  feeds.unshift(newPost);
+  localStorage.setItem("feeds", JSON.stringify(feeds));
+  window.location.href = "feed.html";
+}
 
-    });
-  });
-  
-  const backToFeedButton = document.getElementById('back-to-feed-button') as HTMLElement;
-  backToFeedButton.addEventListener('click', (event) => {
+document.addEventListener("DOMContentLoaded", () => {
+  const formElement = document.getElementById("new-post-form") as HTMLFormElement;
+
+  formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-    window.location.href = "feed.html";
+    const title = (document.getElementById("title") as HTMLInputElement).value;
+    const content = (document.getElementById("content") as HTMLTextAreaElement).value;
+    const author = (document.getElementById("author") as HTMLInputElement).value;
+    const imageFile = (document.getElementById("image") as HTMLInputElement).files?.[0];
+    createNewPost(title, content, author, imageFile);
+    alert('Post submitted');
+  });
+});
 
-  } ) 
+const backToFeedButton = document.getElementById('back-to-feed-button') as HTMLElement;
+backToFeedButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = "feed.html";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Feed } from "../models/userinterface";
+// import { loadFeed } from "../feed/feed";
+// import { feeds } from "../feed/feed";
+// import "./newpost.css"
+
+// function createNewPost(title: string, content: string, author: string, imageFile?: File) {
+//     const newPost: Feed = {
+//         user: author,
+//         title: title,
+//         content: content,
+//       };
+//       if (imageFile) {
+//         const reader = new FileReader();
+//         reader.onload = (event) => {
+//           if (event.target instanceof FileReader) {
+//             newPost.image = event.target.result as string;
+//             feeds.unshift(newPost);
+//             const feedHtml = loadFeed(feeds);
+//             const blogFeedElement = document.getElementById("blog-feed");
+//             if (blogFeedElement) {
+//               blogFeedElement.innerHTML = feedHtml;
+//             }
+//           }
+//         };
+//         reader.readAsDataURL(imageFile);
+//       } else {
+//         feeds.unshift(newPost);
+//         const feedHtml = loadFeed(feeds);
+//         const blogFeedElement = document.getElementById("blog-feed");
+//         if (blogFeedElement) {
+//           blogFeedElement.innerHTML = feedHtml;
+//         }
+//       }
+      
+    
+//       // Store the updated feeds array in the localStorage
+//       localStorage.setItem("feeds", JSON.stringify(feeds));
+    
+//   }
+  
+// ///////
+
+//   document.addEventListener("DOMContentLoaded", () => {
+//     const formElement = document.getElementById("new-post-form") as HTMLFormElement;
+      
+//     formElement.addEventListener("submit", (event) => {
+//       event.preventDefault();
+//       const title = (document.getElementById("title") as HTMLInputElement).value;
+//       const content = (document.getElementById("content") as HTMLTextAreaElement).value;
+//       const author = (document.getElementById("author") as HTMLInputElement).value;
+//       const imageFile = (document.getElementById("image") as HTMLInputElement).files?.[0];
+//       createNewPost(title, content, author, imageFile);
+//       alert('Post submitted'); 
+
+//     });
+//   });
+  
+//   const backToFeedButton = document.getElementById('back-to-feed-button') as HTMLElement;
+//   backToFeedButton.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     window.location.href = "feed.html";
+
+//   } ) 
 
   ////////////////////
+
+
+
+
+
+
+
+
+
 
 
 
